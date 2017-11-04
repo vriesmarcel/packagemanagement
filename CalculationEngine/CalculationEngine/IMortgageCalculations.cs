aacb65c2e545bb5b01dd513776093b5aebeac69c
Marcel de Vries <vriesmarcel@hotmail.com>
2017-11-04 15:58:19 +0000

    Updated IMortgageCalculations.cs

--- CalculationEngine/CalculationEngine/IMortgageCalculations.cs
+++ CalculationEngine/CalculationEngine/IMortgageCalculations.cs
@@ -2,6 +2,7 @@
 {
     public interface IMortgageCalculations
     {
+        // some extra change
         double MortgageRepaymentSummary(double homeValue, double loanAmount, double interestRate, int termInYears);
         double MortgageMaximum(double yearIncome, double homeValue);
     }
